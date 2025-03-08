5f499236f048f85b8a0f712f71fb3415a266de46
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-03-08 16:05:13 +0530

    added blogs.json

---
+++ auto_commit_push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Navigate to your Git repository (replace with your actual path)
+cd ~/repos/dev_journal || exit
+
+# Add all unstaged changes
+git add .
+
+# Commit with a default message including a timestamp
+commit_message="Auto commit on $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "$commit_message"
+
+git fetch
+
+git pull
+# Push changes to the current branch
+git push 
+
+echo "Changes committed and pushed successfully."
+
